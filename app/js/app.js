e22a6f17a564c3d4450653cbc12e3f60b36e9d18
gittypu <gy.puter.payoneer@gmail.com>
2016-06-22 10:18:08 +0300

    dis[lay form removed

--- app/js/app.js
+++ app/js/app.js
@@ -110,7 +110,7 @@ myApp.controller('theropistdata', function ($scope) {
         $http.post('https://api.mlab.com/api/1/databases/speach-theropy/collections/Patient?apiKey=XvABGEjSRBRVhRBHAwKr5XvGS32ARJXw', { 'firstName': $scope.patient.firstName, 'lastName': $scope.patient.lastName, 'phone': $scope.patient.phone, 'EMail': $scope.patient.EMail })
             .then(function(response) {
                 console.log("data entered");
-				$scope.displayForm = false;
+				//$scope.displayForm = false;
                 var promise = patientsService.getPatients();
 	            promise.then(function(data){	
 		        $scope.patients = data.data;
